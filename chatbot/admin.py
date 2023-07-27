189c989cc3fba7f66ad377a3d345d52aa5154b52
su12 <ts04109@naver.com>
2023-07-27 16:48:46 +0900

    chatview admin 연결

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Conversation
 
+admin.site.register(Conversation)
 # Register your models here.
--- chatbot/views.py
+++ chatbot/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from dotenv import load_dotenv
 import openai
 import os
+from .models import Conversation
 
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -10,15 +11,17 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 class ChatView(View):
     def get(self, request, *args, **kwargs):
-        conversations = request.session.get('conversations',[])
+        conversations = request.session.get('conversations', [])
         return render(request, 'chat.html', {'conversations': conversations})
-    
+
     def post(self, request, *args, **kwargs):
         prompt = request.POST.get('prompt')
         if prompt:
+            # 이전 대화 기록 가져오기
             session_conversations = request.session.get('conversations', [])
             previous_conversations = "\n".join([f"User: {c['prompt']}\nAI: {c['response']}" for c in session_conversations])
             prompt_with_previous = f"{previous_conversations}\nUser: {prompt}\nAI:"
+
             model_engine = "text-davinci-003"
             completions = openai.Completion.create(
                 engine=model_engine,
@@ -29,10 +32,13 @@ class ChatView(View):
                 temperature=0.5,
             )
             response = completions.choices[0].text.strip()
-            conversation = {'prompt' : prompt, 'response' : response}
 
-            session_conversations.append(conversation)
+            conversation = Conversation(prompt=prompt, response=response)
+            conversation.save()
+
+            # 대화 기록에 새로운 응답 추가
+            session_conversations.append({'prompt': prompt, 'response': response})
             request.session['conversations'] = session_conversations
+            request.session.modified = True
 
         return self.get(request, *args, **kwargs)
-
