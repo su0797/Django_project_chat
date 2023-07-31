499a0274cb246856d251e83d42f6d20bdc19ba6a
su12 <ts04109@naver.com>
2023-08-01 00:21:44 +0900

    ChatView 수정 후 연결

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,34 +1,26 @@
-from django.shortcuts import render
-from django.views import View
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from dotenv import load_dotenv
 import openai
 import os
 from .models import Conversation
 
-
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-
-class ChatView(View):
+class ChatView(APIView):
     def get(self, request, *args, **kwargs):
         conversations = request.session.get('conversations', [])
-        return render(request, 'chat.html', {'conversations': conversations})
-
-    # def post(self, request):
-    #     chat = request.data
-    #     serializer = self.serializer_class(data=chat)
-    #     serializer.is_valid(raise_exception=True)
-    #     return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'conversations': conversations}, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
-        prompt = request.POST.get('prompt')
+        prompt = request.data.get('prompt')
         if prompt:
             # 이전 대화 기록 가져오기
             session_conversations = request.session.get('conversations', [])
-            previous_conversations = "\n".join([f"User: {c['prompt']}\nAI: {c['response']}" for c in session_conversations])
-            prompt_with_previous = f"{previous_conversations}\nUser: {prompt}\nAI:"
+            previous_conversations = "\n".join([f"사용자: {c['prompt']}\nAI: {c['response']}" for c in session_conversations])
+            prompt_with_previous = f"{previous_conversations}\n사용자: {prompt}\nAI:"
 
             model_engine = "text-davinci-003"
             completions = openai.Completion.create(
@@ -49,8 +41,9 @@ class ChatView(View):
             request.session['conversations'] = session_conversations
             request.session.modified = True
 
-        return self.get(request, *args, **kwargs)
+            return Response({'response': response}, status=status.HTTP_201_CREATED)
 
+        return Response({'error': '입력 내용이 필요합니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
 # def chatbot(request):
     # 이전 코드 내용을 그대로 유지하고, 마지막에 JsonResponse로 응답을 반환합니다.
