bcca81603d5c0b437a3be6c2d3321c3abf9f9c7c
su12 <ts04109@naver.com>
2023-09-03 11:20:27 +0900

    feat : blog List 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from .models import Post
 # Create your views here.
+
+User = get_user_model
+
+
+class List(APIView):
+    def get(self, request):
+        posts = Post.objects.order_by('created_at')
+
+    
