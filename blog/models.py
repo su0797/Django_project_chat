91ec543ce28f4b0d8fa4b2401bf1d9d08123cf68
su12 <ts04109@naver.com>
2023-09-29 16:20:25 +0900

    fix : post model 수정

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ User = get_user_model
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    writer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
@@ -17,7 +17,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
     content = models.TextField()
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    writer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -28,7 +28,7 @@ class Comment(models.Model):
 class HashTag(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    writer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- user/models.py
+++ user/models.py
@@ -43,7 +43,6 @@ class User(AbstractUser):
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     last_login = models.DateTimeField(null=True, blank=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
     
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
