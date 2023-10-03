0990ab11c6684446e865915fac1b5895e6cd40f8
su12 <ts04109@naver.com>
2023-10-03 17:40:39 +0900

    refactor : blog url 수정

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import List, Write, Edit, Delete,  View, CommentWrite, CommentDelete
+from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
     # 게시글
-    path("", List.as_view(), name='list'),
-    path("write/", Write.as_view(), name='write'),
-    path('edit/<int:pk>/', Edit.as_view(), name='edit'),
-    path('delete/<int:pk>/', Delete.as_view(), name='delete'),
-    path('view/<int:pk>/', View.as_view(), name='view'),
-    path('comment/write/', CommentWrite.as_view(), name='cm-write'),
-    path('comment/delete/', CommentDelete.as_view(), name='cm-delete'),
+    path("", views.List.as_view(), name='list'),
+    path("write/", views.Write.as_view(), name='write'),
+    path('edit/<int:pk>/', views.Edit.as_view(), name='edit'),
+    path('delete/<int:pk>/', views.Delete.as_view(), name='delete'),
+    path('view/<int:pk>/', views.View.as_view(), name='view'),
+    path('comment/write/', views.CommentWrite.as_view(), name='cm-write'),
+    path('comment/delete/', views.CommentDelete.as_view(), name='cm-delete'),
     # 대댓글 기능 추가하기
 ]
